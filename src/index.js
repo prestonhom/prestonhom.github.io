1a9ac7eb7b5f47fd730e3effee3953b303e8c816
Preston Hom <Preston@Prestons-MacBook-Air.local>
2019-10-25 14:27:52 -0700

    import hashrouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 ReactDOM.render(
   <Router basename="/">
   <App />
